21fbbbca813b0d7c1f4e00407b2ff0c876359a26
matnij-4 <hej.ma5ttias@gmail.com>
2019-11-11 09:06:46 +0100

    Initial commit

---
+++ bSort.py
@@ -0,0 +1,117 @@
+# bSort an insertyionsort with binary search
+import random
+import timeit
+
+
+def bin_search(a, x, high):
+    low = 0
+
+    while True:
+        if low == high:
+            if a[low] < x:
+                return low + 1
+            else:
+                return low
+
+        if low > high:
+            return low
+
+        mid = (high + low) // 2
+
+        if x < a[mid]:
+            high = mid - 1
+        elif x > a[mid]:
+            low = mid + 1
+        else:
+            return mid
+
+        
+def insertsort(a):
+    for i in range(1, len(a)):
+        x = a[i]
+        for n in range(0, i):
+            if a[n] > x:
+                a = a[:n] + [x] + a[n:i] + a[i+1:]
+
+    return a
+
+
+def bSort(ilist):
+    
+    for i in range(1, len(ilist)):
+        x = ilist[i]
+        pos = bin_search(ilist, x, i - 1)
+        ilist = ilist[:pos] + [x] + ilist[pos:i] + ilist[i+1:]
+
+    return ilist
+
+
+def merge(al, bl):
+    cl = []
+    while len(al) > 0 and len(bl) > 0:
+        if al[0] < bl[0]:
+            cl.append(al[0])
+            al = al[1:]
+        else:
+            cl.append(bl[0])
+            bl = bl[1:]
+
+    if len(bl) > 0:
+        cl += bl
+    else:
+        cl += al
+
+    return cl
+
+def binMergeSort(l, k):
+    lenght = len(l)
+    if lenght <= k:
+        return bSort(l)
+
+    al = binMergeSort(l[:lenght // 2  +1], k)
+    bl = binMergeSort(l[lenght // 2  +1:], k)
+
+    return merge(al, bl)
+
+def insMergeSort(l, k):
+    lenght = len(l)
+    if lenght <= k:
+        return insertsort(l)
+
+    al = binMergeSort(l[:lenght // 2  +1], k)
+    bl = binMergeSort(l[lenght // 2  +1:], k)
+
+    return merge(al, bl)
+
+def isSorted(a):
+    last = a[0]
+    for x in a[1:]:
+        if x < last:
+            return False
+        last = x
+
+    return True
+    
+    
+        
+# testcode
+flag = True
+random.seed()
+
+def rndlist(n):
+    a = []
+    for n in range(0, n):
+        a.append(random.randint(0, 10000))
+    return a
+
+
+
+print("binMergesort timing:")
+for x in range(1000, 50001, 200):
+    time = timeit.timeit('a = binMergeSort(a, 100)', globals=globals(), setup="a = rndlist(x)", number=5) 
+    print(str(x) + "    " + str(time))
+
+print("insMergeSort timing:")
+for x in range(1000, 50001, 200):
+    time = timeit.timeit('a = insMergeSort(a, 100)', globals=globals(), setup="a = rndlist(x)", number=5) 
+    print(str(x) + "    " + str(time))
---
+++ bSort_new.py
@@ -0,0 +1,76 @@
+import time
+import random
+# Help function to find the place for the insert sort function.
+def findPlace(array, value, start, end):
+
+    # Base the fist time it just add the number.
+    if start == end:
+        if array[start] > value:
+            return start
+        else:
+            return start+1
+
+
+    # Return start position if we wen to far.
+    if start > end:
+        return start
+
+    
+    # By converting to an it it becomes Floor.
+    mid = int((start+end)/2)
+
+
+    # If it is is bigger make a new jump
+    if array[mid] < value:
+        return findPlace(array, value, mid+1, end)
+
+    # If it is more then make a new jummp.
+    elif array[mid] > value:
+        return findPlace(array, value, start, mid-1)
+
+    # If it is exat you can place it there.
+    else:
+        return mid
+
+
+
+# Function takes a list of Ints and returns it Sorted.
+def bSort(array):
+
+    for i in range(1,len(array)):
+        
+        # Pick out the value first.
+        value = array[i]
+        
+        # Find the place is handeld by the Binary search called findPlace
+        insertIndex = findPlace(array, value, 0 , i-1)
+        
+        #Place the nummber in the right position.
+        array = array[:insertIndex] + [value] + array[insertIndex:i] + array[i+1:]
+
+    return array
+
+
+def randomlist(n):
+    array = []
+
+    for n in range(0,n):
+        array.append(random.randint(0,10000))
+
+    return array
+
+
+
+# Short array list with some ints.
+array = [5,2,3,6,7,8,10,11,9,12,18,19,5677]
+array = randomlist(10000)
+ts = time.time()
+print(bSort(array))
+ts = (time.time() - ts)
+print ("It took " + str(ts) + " sec to complete")
+print("\n\n\n")
+
+
+
+
+
